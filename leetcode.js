1e51bf0810b6ba841adb2fc993951a8454f7c9fe
Yuxinl1997 <95650053+Yuxinl1997@users.noreply.github.com>
2021-12-22 20:52:02 -0500

    Create leetcode.js

---
+++ leetcode.js
@@ -0,0 +1,62 @@
+//1
+var twoSum = function(nums, target) {
+    for (var i = 0; i< nums.length; i++) {
+        var difference = target - nums[i];
+        for (var j = i +1; j<nums.length; j++) {
+            if (nums[j] == difference)
+                return [i,j];
+        }
+    }
+};
+
+//9
+var isPalindrome = function(x) {
+    var str  = x.toString();
+    for(var i = 0;i <= str.length -1;i++){
+        if(str[i] != str[str.length - 1 - i]){
+            return false;
+        }
+        }
+        return true
+ };
+
+//13
+var romanToInt = function(s) {
+    let o = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        },
+        n = 0;
+    for(let i = 0,length = s.length; i < length; i++){
+        const temp = o[s[i]]
+        if( i < length - 1 &&  temp < o[s[i+1]] ){
+            n -= temp
+        }else{
+            n += temp
+        }
+    }
+    return n
+};
+
+//20
+var isValid = function (s) {
+    while (s.length) {
+        var temp = s;
+        s = s.replace('()', '');
+        s = s.replace('[]', '');
+        s = s.replace('{}', '');
+        if (s == temp) return false
+    }
+    return true;
+};
+
+//58
+const lengthOfLastWord = s => {
+    const arr = s.split(' ').filter(item => item != '');
+    return arr[arr.length - 1].length;
+};
